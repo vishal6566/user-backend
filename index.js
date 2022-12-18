f14ba03e19f36d59d686429524667a6232ef1cb6
Vishal Das <106031563+vishal6566@users.noreply.github.com>
2022-12-18 22:58:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const app=express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors()=>{
+origin:"*"
+})
 const PORT= process.env.PORT||5000
 app.get("/",(req,res)=>{
     res.send("homepage")
