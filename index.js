ee6b8534a4a52c332714fe9c9bd9f5c2d37465d5
Vishal Das <106031563+vishal6566@users.noreply.github.com>
2022-12-18 23:04:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const connect=require("./config/db")
 const userRoute=require("./routes/userRouter")
 
 
-const {productRouter }  = require("./routes/productRouter")
-const { pr_connection } = require("./config/pr_db")
+
+
 const app=express()
 
 app.use(express.json())
@@ -20,7 +20,7 @@ const PORT= process.env.PORT||5000
 app.get("/",(req,res)=>{
     res.send("homepage")
 })
-app.use("/products", productRouter)
+
 //routes middleware
 app.use("/api/users",userRoute)
 
